e2123dbd581f2f8fc9874ebedf265727a44f2e4b
javacasm <javacasm@gmail.com>
2020-06-17 11:10:16 +0200

    update

--- RiegoRaspberryArduino/RiegoRaspberryArduino.ino
+++ RiegoRaspberryArduino/RiegoRaspberryArduino.ino
@@ -1,10 +1,33 @@
+/*
+ *   Sistema de control de riego 
+ * 
+ *   *  Control de rel'es de electrov'alvulas y rel'e bomba de riego
+ *   *  Medida de condiciones atmosf'ericas: Temperatura, humedad y presi'on
+ *   *  Se muestra el estado en un lcd 
+ *   *  Se env'ia el estado bajo petici'on por el puerto serie
+ * 
+ *   Pensado para que lo controle una Rapsberry Pi
+ * 
+ *   Licencia CC by SA by @javacasm
+ *   
+ *   Junio de 2020
+ * 
+ */
+
+
 // defines
 
-#define VERSION "0.1"
-#define SEPARADOR ";"
+#define VERSION "V:0.3"
+#define SEPARADOR_SERIE ";"
+
 #define PERIODO_SENSORES 5000
-#define CMD_DATA2SERIE "*"
-#define CMD_PIN2STATE "S"
+
+#define END_OF_COMMAND '\n'
+#define CMD_HELP       'h'
+#define CMD_DATA2SERIE '*'
+#define CMD_PIN2STATE 'S'
+#define CMD_PIN_HIGH  'H'
+#define CMD_PIN_LOW   'L'
 
 #define mostrarMensaje(x) Serial.println(x); lcd.print(x);
 
@@ -22,8 +45,8 @@ BME280I2C bme;
 
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
-#define NReles  4
-int reles[] = {2, 3, 4, 5};
+#define NReles  5
+int reles[] = {2, 3, 4, 5,6};
 
 float presion, temperatura, humedad;
 
@@ -39,7 +62,7 @@ void setup() {
   Serial.begin(115200);
 
   
-  mostrarMensaje(F(VERSION));
+  mostrarMensaje(String(VERSION) + "by @javacasm CC by SA");
 
   // Configuracion Sensor BME280
   Wire.begin();
@@ -49,6 +72,7 @@ void setup() {
 
   for( int i = 0; i < NReles; i++){
     pinMode(reles[i], OUTPUT);
+    digitalWrite(reles[i], HIGH);
   }
 
   tiempoUltimoDato = millis();
@@ -69,14 +93,58 @@ void loop() {
 
 void leerPuertoSerie(){
   while (Serial.available() > 0) {
-    char lectura = Serial.read();
-
+    char data;
+    String command = "";
+    while ( (data = Serial.read() ) != END_OF_COMMAND){
+      command += data;
+    }
+    Serial.print("<<");
+    Serial.println(command);
+    execCommand(command);
   }
 
   
 }
 
+void changePin2State(String command){
+  int indexPin = command[1] - '1';
+  bool newState = command[2] == CMD_PIN_HIGH ? HIGH  : LOW;
+  digitalWrite(reles[indexPin],newState);
+  Serial.print(">> ");
+  Serial.print(reles[indexPin]);
+  Serial.println(command[2]);
+}
 
+void showHelp(){
+  Serial.println("Comandos:");
+  Serial.print(CMD_HELP);
+  Serial.println(" show help");
+  Serial.print(CMD_DATA2SERIE);
+  Serial.println(" send data to serial");
+  Serial.print(CMD_PIN2STATE);
+  Serial.println("Xs X:pinID s: H for HIGH, L for LOW");
+  Serial.println("\\n to send command");
+
+}
+
+void execCommand(String command) {
+  switch(command[0]){
+    case CMD_DATA2SERIE:
+      mostrarDatosSerie();
+      break;
+    case CMD_PIN2STATE:
+      changePin2State(command);
+      break;
+    case CMD_HELP:
+      showHelp();
+      break;
+    default:
+      mostrarMensaje(command);
+      break;
+    
+  }
+
+}
 
 void leerSensores(){
    BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
@@ -86,41 +154,41 @@ void leerSensores(){
 
 void mostrarDatosSerie(){ 
   Serial.print(temperatura);
-  Serial.print(F(SEPARADOR));
+  Serial.print(F(SEPARADOR_SERIE));
   Serial.print(int(round(humedad)));
-  Serial.print(F(SEPARADOR));
+  Serial.print(F(SEPARADOR_SERIE));
   Serial.print(long(presion));
+  Serial.print(F(SEPARADOR_SERIE));
   for(int i = 0; i < NReles; i++){
-    if (digitalRead(reles[i]))
+    if (!digitalRead(reles[i]))
         Serial.print("On;");
     else
         Serial.print("Off;");
+    Serial.print(F(SEPARADOR_SERIE));
   }  
+  Serial.println();
 }
 
 void mostrarDatosLCD(){
-  lcd.clear();
+//  lcd.clear();
   
   /* FORMATEO de datos y ENVIO al puerto SERIE */
   lcd.setCursor(0,0);
   lcd.print(F("T:"));
   lcd.print(temperatura);
-  lcd.print(F(" H:"));
+  lcd.print(F("C H:"));
   lcd.print(int(round(humedad)));
-  lcd.print(F(" P:"));
-  lcd.print(long(presion));
-  //lcd.print(F("atm"));
+  lcd.print(F("%"));
   lcd.setCursor(0,1);
+  lcd.print(F("P:"));
+  lcd.print(long(presion));
+  lcd.print(F("atm"));
+  lcd.setCursor(0,3);
   for( int i = 0; i < NReles; i++){
-    if (digitalRead(reles[i]))
+    if (!digitalRead(reles[i]))
         lcd.print("On  ");
     else
         lcd.print("Off ");
   }
 
-}
-
-void execCommand(String command) {
-  if (command == "*" ) mostrarDatosSerie();
-  
 }
