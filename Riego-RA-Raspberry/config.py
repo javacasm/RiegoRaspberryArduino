7ad3177fcd21b67c243dba661ce4fac24fe7d31a
javacasm <javacasm@gmail.com>
2020-06-18 12:56:39 +0200

    update

--- Riego-RA-Raspberry/config.py
+++ Riego-RA-Raspberry/config.py
@@ -2,12 +2,19 @@
 
 # Telegram Bot Authorization Token
 
-TELEGRAM_API_TOKEN = 'YOUR API TOKEN'
+TELEGRAM_API_TOKEN = 'YOUR TELEGRAM API TOKEN'
 
+v = '0.9'
+
+
+Sectores = range(1,5)
 
 ReleBomba = 5
 
-Sectores = range(1,5)
+SectorPalet = 1
+SectorMacetones = 2
+SectorMacetas = 3
+
 
 separadorDatosArduino = b';'
 
@@ -26,4 +33,4 @@ TimeoutRespuesta = 1
 
 VELOCIDAD_PUERTO_SERIE = 115200
 
-v = '0.75'
+
