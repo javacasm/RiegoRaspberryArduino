b5e0f5bfd408eb38916b280ed71b2b6a0e01bd5b
javacasm <javacasm@gmail.com>
2020-06-23 00:09:42 +0200

    update

--- Riego-RA-Raspberry/TestCamara.py
+++ Riego-RA-Raspberry/TestCamara.py
@@ -1,16 +1,10 @@
 # Test de cmara
 
-from picamera import PiCamera
+import camara
 from time import sleep
-from datetime import datetime
-import config
 
-camera = PiCamera() # creamos el objeto camara
+camera = camara.initCamera() # creamos el objeto camara
 
-camera.start_preview() # muestra la previsualizacion
-sleep(1) # espera 5 segundos
-now = datetime.now() 
-date_time = now.strftime("%Y%d%m-%H%M%S")
-print("date and time:",date_time)
-camera.capture(config.ImagesDirectory + 'image.jpg') # guarda la imagen
-camera.stop_preview() # cierra la previsualizacion
+for i in range(1, 3):
+    camara.getImage(camera)
+    sleep(1)
